03c395897e0e5c8177434d497ab1ca49dc9150b3
davy engone <davy@philos.io>
2015-10-11 21:41:02 +0200

    simple implementation of the add function using ES5

---
+++ wallaby.js
@@ -0,0 +1,25 @@
+var babel = require('babel');
+
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'arrow-function/*.js'
+    ],
+
+    tests: [
+      'test/*spec.js'
+    ],
+
+    env: {
+      type: 'node'
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        babel: babel,
+        // https://babeljs.io/docs/usage/experimental/
+        stage: 0
+      })
+    }
+  };
+};
