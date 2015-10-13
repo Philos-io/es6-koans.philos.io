ae7365d5d1e7162af2455f45f58eb9aa366382e8
davy engone <davy@philos.io>
2015-10-13 17:50:17 +0200

    fixed tests for arrow function

--- test/arrow-function.spec.js
+++ test/arrow-function.spec.js
@@ -3,27 +3,13 @@ import sinon from 'sinon';
 let expect = chai.expect;
 chai.should();
 
-import {add} from '../arrow-function/index.js';
+// You can implement your solution in another file or inline here
 
 // You can find more information about arrow-function mdn by following the link below
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
 describe('Arrow function', () => {
-  describe('Add, sum...', () => {
-
-    let add = (...args) => {
-      if (args.length < 2) {
-        throw new Error('You should pass at least 2 arguments');
-      }
-
-      return args.reduce((x, y) => {
-        if(!isNaN(x) && !isNaN(y))
-          return x+y;
-        else
-          throw new Error('You can only sum numbers');
-      });
-    }
-
+  xdescribe('Add or Sum function...', () => {
     it('Should return 5 when passing 2 and 3', ()=>{
       add(2,3).should.equal(5);
     });
@@ -34,7 +20,7 @@ describe('Arrow function', () => {
         }.should.throw();
     });
 
-    it('Should accept more than 3 arguments', ()=>{
+    it('Should accept more than 2 arguments', ()=>{
       add(1,2,3,4).should.equal(10);
     });
 
