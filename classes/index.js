6f630b08e5c9e3a85da6199d347828f8d82e781f
davy engone <davy@philos.io>
2015-10-13 17:24:39 +0200

    added tests for class in ES6: more tests to come

---
+++ classes/index.js
@@ -0,0 +1,23 @@
+
+function Human({fullName}){
+  if(!fullName) throw new Error('fullName is mandatory');
+
+  Object.defineProperty(this, 'fullName', {
+   get: function() {
+     return fullName;
+   },
+   set: function(value) {
+     if (typeof value !== 'string') {
+       throw new Error('fullName should be a string');
+     }
+     fullName = value;
+   }
+ });
+ 
+ this.fullName = fullName;
+}
+Human.inject = [];
+Human.prototype.Walk = function(){}
+
+
+export default Human;
--- test/classes.spec.js
+++ test/classes.spec.js
@@ -1,26 +1,52 @@
-// var assert = require('assert');
-//
-// describe('Classes in ES6', function(){
-//  describe('User class', function(){
-//
-//    it('should be a function', function(){
-//      assert.equal(1, 2, 'this is a test');
-//    });
-//
-//    it('should have a name property called USER', function(){
-//
-//    });
-//
-//    it('Should have a property firstname of type string');
-//
-//
-//    it('Should have a property lastname of type string');
-//
-//    it('Should have a static property called run and it should be a function');
-//  });
-//
-//
-//  describe('Person class', function(){
-//
-//  });
-// });
+import chai from 'chai';
+import sinon from 'sinon';
+let expect = chai.expect;
+chai.should();
+
+// You can implement your solution in another file or inline here
+
+describe('Class in ES6', ()=>{
+ xdescribe('Foo class', ()=>{
+
+   it('should be a function', ()=>{
+     expect(Human).to.be.a('function');
+   });
+
+   it('should have a name property called Human', ()=>{
+     expect(Human).has.property('name').equal('Human');
+   });
+
+   it('Should have a property prototype of type object', ()=>{
+     expect(Human).has.property('prototype').is.a('object');
+     expect(typeof Human.prototype).equal('object');
+
+     describe('Human.prototype', ()=>{
+       it('Should contain a property Walk of type function', ()=>{
+        expect(Human.prototype).has.property('Walk').is.a('function');
+       });
+     });
+   });
+
+   it('Should contain a static property called inject of type array', ()=>{
+     expect(Human).has.property('inject').is.an('array');
+   });
+
+   it('Should throw an error if no fullName is passed at the instantiation', ()=>{
+     () => {
+        new Human()
+      }.should.throw(Error);
+   })
+   // Getter and setter in ES6 class
+   it('Should have an instance property fullName', ()=>{
+     let obj = new Human({fullName: 'Tom Thomas'});
+     expect(obj.fullName).to.be.defined;
+     expect(obj.fullName).to.equal('Tom Thomas');
+   });
+
+   it('It should throw an error if fullName is set to anything else but a string', ()=>{
+     () => {
+        new Human({fullName: 12987})
+      }.should.throw(Error);
+   });
+ });
+});
