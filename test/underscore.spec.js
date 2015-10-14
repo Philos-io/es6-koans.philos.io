04587557924548dddde5177fdd498bdd0bc94bdb
davy engone <davy@philos.io>
2015-10-14 07:32:53 +0200

    added tests for identity and first

--- test/underscore.spec.js
+++ test/underscore.spec.js
@@ -1,14 +1,39 @@
 import chai from 'chai';
-let expect = chai.expect();
+let expect = chai.expect;
+chai.should();
 
-import * as _ from '../underscore/'
+import * as _ from '../src/underscore'
+
+console.log(_);
 
 describe('Underscore library', ()=>{
 
     describe('identity', ()=>{
+      it('Should return the same value that has been passed', ()=>{
+        expect(_.identity(1)).equal(1);
+        expect(_.identity({})).to.deep.equal({});
+        expect(_.identity("philos")).to.deep.equal("philos");
+      })
+    });
 
-      it('Should return', ()=>{
-        _.identity
+    describe('first', ()=>{
+
+      let items = [1,9, 10, undefined];
+
+      it('Should throw an error if nothing is passed', ()=>{
+        ()=>{
+          _.first();
+        }.should.throw(Error);
+      });
+
+      it('Should return the first element of the collection', ()=>{
+        expect(_.first(items)).equal(1);
+      });
+
+      it('Should return the first n elements when a second arguments is passed', ()=>{
+        expect(_.first(items, 0)).equal(1);
+        expect(_.first(items, 1)).to.deep.equal([1]);
+        expect(_.first(items, 2)).to.deep.equal([1, 9]);
       })
     });
 });
