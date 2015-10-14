b867075cdda90b869fc332e9a1998918fc434529
davy engone <davy@philos.io>
2015-10-14 07:04:32 +0200

    spec for underscore

---
+++ test/underscore.spec.js
@@ -0,0 +1,14 @@
+import chai from 'chai';
+let expect = chai.expect();
+
+import * as _ from '../underscore/'
+
+describe('Underscore library', ()=>{
+
+    describe('identity', ()=>{
+
+      it('Should return', ()=>{
+        _.identity
+      })
+    });
+});
