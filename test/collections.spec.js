1fc38e13723707208708298c6fe01945779f5190
davy engone <davy@philos.io>
2015-10-21 23:38:07 +0200

    added few specs for collections

---
+++ test/collections.spec.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+let expect = chai.expect;
+
+
+describe('Collection', ()=>{
+  describe('Set', ()=>{
+    it('')
+  });
+
+  describe('WeakMap', ()=>{
+    it('')
+  });
+
+  describe('Map', ()=>{
+    it('')
+  });
+
+
+});
