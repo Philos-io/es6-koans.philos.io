6f4985f784329ace352206942cfd9f77d8d503ad
davy engone <davy@philos.io>
2015-11-01 04:53:34 +0100

    renamed function to sum

--- src/arrow-function/index.js
+++ src/arrow-function/index.js
@@ -1,25 +1,19 @@
-/**
- ** Represents a simple operation.
- ** @function
- ** @param {numbers} args -  List of numbers.
- ** @return {number} returnValue - sum of a list of numbers
- **/
+let sum = (...args) => {
 
-export function add(){
-  if(arguments.length < 2){
-    throw new Error('You should pass at least 2 arguments');
+  if (args.length < 2) {
+    throw new Error();
   }
+  
+  var cumul = 0;
+  for(var i=0; i<args.length; i++) {
+    var num = args[i];
 
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    if(isNumber(arguments[i]))
-      sum += arguments[i];
-    else
-      throw new Error('You can only sum numbers');
+    if (isNaN(num)) {
+      throw new Error();
+    }
+    cumul+=num;
   }
-  return sum;
+  return cumul;
 }
 
-function isNumber(n) {
-  return !Number.isNaN(n) && Number.isFinite(n);
-}
+export default sum;
