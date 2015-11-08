18343d9622af1ae101546a53a7caeab2b5321db7
davy engone <davy@philos.io>
2015-11-08 17:47:35 +0100

    added idea folder to .gitignore and fixed issue with default import in arrow function

--- src/arrow-function/index.js
+++ src/arrow-function/index.js
@@ -1,9 +1,12 @@
+
+export default sum;
+
 let sum = (...args) => {
 
   if (args.length < 2) {
     throw new Error();
   }
-  
+
   var cumul = 0;
   for(var i=0; i<args.length; i++) {
     var num = args[i];
@@ -15,5 +18,3 @@ let sum = (...args) => {
   }
   return cumul;
 }
-
-export default sum;
