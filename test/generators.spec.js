0a0811b7ed10e25156d456a4e48cb03eefae0cf9
davy engone <davy@philos.io>
2015-12-02 18:50:54 +0100

    added few tests for generators

---
+++ test/generators.spec.js
@@ -0,0 +1,19 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+// http://koajs.com/
+
+import chai from 'chai';
+let expect = chai.expect;
+
+
+class Company {
+  constructor(customers){
+    this.customers = customers;
+  }
+}
+
+describe('Generators', ()=>{
+  it('Company should be iterable by customers', ()=>{
+    let company = new Company(['Davy', 'Thomas', 'Lars']);
+    expect([...company]).to.deep.equal(['Davy', 'Thomas', 'Lars']);
+  });
+});
